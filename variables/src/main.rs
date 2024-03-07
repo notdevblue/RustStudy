bc9b1c9cf46f4ab4ee9622543114b8e7333abc30
HanAtWork <wooyeophan@gmail.com>
2024-03-07 18:18:42 +0900

    러스트 변수와 가변성 하는 중

---
+++ variables/src/main.rs
@@ -0,0 +1,20 @@
+// https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html
+
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+fn main() {
+    // let mut x = 5;
+    // println!("The value of x is: {x}");
+    // x = 6;
+    // println!("The value of x is: {x}");
+
+    let x = 5;
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+}
