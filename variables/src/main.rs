9f24b29b89c917debab8c30729efb2b583780e31
HanWSLGentoo <wooyeophan@gmail.com>
2024-03-14 00:40:49 +0900

    러스트 변수와 불변성

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,20 +1,37 @@
+
 // https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html
-
-const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-
-fn main() {
+// const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+fn variables() {
     // let mut x = 5;
     // println!("The value of x is: {x}");
     // x = 6;
     // println!("The value of x is: {x}");
 
-    let x = 5;
-    let x = x + 1;
+    // let x = 5;
+    // let x = x + 1;
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
+    // {
+    //     let x = x * 2;
+    //     println!("The value of x in the inner scope is: {x}");
+    // }
 
-    println!("The value of x is: {x}");
+    // println!("The value of x is: {x}");
+
+    // let spaces = "   ";
+    // let spaces = spaces.len();
+    // println!("The spaces: {spaces}");
+
+    // let mut spaces = "   ";
+    // spaces = spaces.len(); // expected &str found usize
+}
+
+// https://doc.rust-lang.org/book/ch03-02-data-types.html
+fn datatypes() {
+    
+}
+
+
+fn main() {
+    // variables();
+    datatypes();
 }
