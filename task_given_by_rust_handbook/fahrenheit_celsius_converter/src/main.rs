6f8896125049a37e9f9f64bbbe8e677d860e89bf
HanAtWork <wooyeophan@gmail.com>
2024-03-27 17:58:43 +0900

    섭씨화씨 변환기

--- task_given_by_rust_handbook/fahrenheit_celsius_converter/src/main.rs
+++ task_given_by_rust_handbook/fahrenheit_celsius_converter/src/main.rs
@@ -1,7 +1,7 @@
 // https://blog.naver.com/kgs_safety/221928059157
 // 변환법 블로그
 
-use std::io;
+use std::io::{self, stdin};
 
 fn main() {
     loop {
@@ -13,7 +13,7 @@ fn main() {
             .read_line(&mut buffer)
             .expect("Failed to read line");
         
-        let parsed: i32 = match buffer.parse() {
+        let parsed: i32 = match buffer.trim().parse() {
             Ok(parsed_result) => parsed_result,
             Err(_) => {
                 println!("Invalid input");
@@ -30,16 +30,36 @@ fn main() {
             }
         };
 
-        println!("parsed result: {result}");
+        println!("Converted result is: {result}");
         break;
     }
 
 }
 
-fn fahrenheit_to_celcius() -> i32 {
-    0
+fn fahrenheit_to_celcius() -> f32 {
+    (value_input() - 32) as f32 / 1.8
 }
 
-fn celcius_to_fahrenheit() -> i32 {
-    0
+fn celcius_to_fahrenheit() -> f32 {
+    (value_input() as f32 * 1.8) + 32.0
+}
+
+fn value_input() -> i32 {
+    loop {
+        let mut buffer = String::new();
+
+        println!("Input value.");
+
+        stdin().read_line(&mut buffer).expect("Failed to read line");
+
+        let result: i32 = match buffer.trim().parse() {
+            Ok(value) => value,
+            Err(_) => {
+                println!("Invalid input");
+                continue;
+            }
+        };
+
+        return result;
+    }
 }
