8a100bea8c38860963f0354e110fc893eac7536a
HanAtWork <wooyeophan@gmail.com>
2024-03-27 18:27:31 +0900

    피보나치 수열

--- task_given_by_rust_handbook/nth_fibonacci/src/main.rs
+++ task_given_by_rust_handbook/nth_fibonacci/src/main.rs
@@ -1,3 +1,39 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut buf = String::new();
+
+        println!("Input you number");
+
+        io::stdin().read_line(&mut buf).expect("Failed to read line");
+
+        let n: i64 = match buf.trim().parse() {
+            Ok(res) => res,
+            Err(_) => continue
+        };
+
+        println!(
+            "{}{} fibonacci number is {}",
+            n,
+            match n {
+                1 => "st",
+                2 => "nd",
+                3 => "rd",
+                _ => "th"
+            },
+            fib(n)
+        );
+        break;
+    }
+}
+
+fn fib(n: i64) -> i64 {
+    if n <= 0 {
+        0
+    } else if n == 1 {
+        1
+    } else {
+        fib(n - 1) + fib(n - 2)
+    }
 }
