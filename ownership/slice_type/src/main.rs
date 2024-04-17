8f02235bc188153d1577a9d8b14a3458f89f1a50
HanAtWork <wooyeophan@gmail.com>
2024-04-17 15:08:52 +0900

    슬라이스 연산자

--- ownership/slice_type/src/main.rs
+++ ownership/slice_type/src/main.rs
@@ -1,8 +1,6 @@
 // https://doc.rust-lang.org/book/ch04-03-slices.html
 /*
 slices 는 컬렉션의 연속되는 요소들을 컬랙션 통째를 가지는 것 대신 레퍼런스 할 수 있게 해줌
-
-
 */
 
 fn main() {
@@ -11,13 +9,46 @@ fn main() {
 }
 
 fn string_slices() {
-    // https://doc.rust-lang.org/book/ch04-03-slices.html#string-slices
-    // string slice는 String 일부분의 레퍼런스임
-    let s = String::from("Install Gentoo");
-    let install = &s[0..7];
-    let gentoo = &s[8..13];
+    // // https://doc.rust-lang.org/book/ch04-03-slices.html#string-slices
 
-    println!("{}{}", install, gentoo);
+    // // string slice는 String 일부분의 레퍼런스임
+    // let s = String::from("Install Gentoo");
+    // let install = &s[0..7];
+    // let gentoo = &s[8..14];
+    // // [inclusive..exclusive]
+    // // 복사해서 가지는것이 아닌
+    // // 원레 오브젝트를 가리키는 포인터 형식임
+    // // [8..14] 라면, s[8] 부터 6 (14 - 8)만큼의 길이를 가지는 슬라이스임
+    // // { ptr-> s[8], len -> 6 }
+    // println!("{}{}", install, gentoo);
+
+    // let len = s.len();
+
+    // // 0부터 시작하고 싶으면 앞에 시작 인덱스는 없어도 됨
+    // let slice1 = &s[0..3];
+    // let slice2 = &s[..3];
+    // // 둘다 같은 의미
+    // println!("{} {}", slice1, slice2);
+
+    // // 반대로 끝까지 가고 싶으면 뒤에 인덱스도 없어도 됨
+    // let slice1 = &s[8..len];
+    // let slice2 = &s[8..];
+    // // 둘다 같은 의미
+    // println!("{} {}", slice1, slice2);
+
+    // // 앞뒤 둘다 없엘수도 있음
+    // let slice1 = &s[0..len];
+    // let slice2 = &s[..];
+    // // 둘다 같은 의미
+    // println!("{} {}", slice1, slice2);
+
+    // // string slice 인덱스는 UTF-8 문자 범위 내에서 사용되어야 함
+    // // 멀타바이트 문자에서 사용한다면 터짐
+    // // let multibyte = String::from("؃ ؃ ؃ ");
+    // // let b = &multibyte[..1];
+    // // println!("{b}");
+    // // 이 섹션에선 ascii 문자만 사용한다고 가정
+
 }
 
 fn tedious_way() {
