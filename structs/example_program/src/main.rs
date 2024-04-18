2e12fdf9912041db1c89eff97cf162dee5fbc233
HanAtWork <wooyeophan@gmail.com>
2024-04-18 18:30:53 +0900

    예시 프로그램 기반

--- structs/example_program/src/main.rs
+++ structs/example_program/src/main.rs
@@ -1,3 +1,15 @@
+// https://doc.rust-lang.org/book/ch05-02-example-structs.html
+
 fn main() {
-    println!("Hello, world!");
+    let width1 = 30;
+    let height1 = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels",
+        area(width1, height1)
+    );
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
 }
