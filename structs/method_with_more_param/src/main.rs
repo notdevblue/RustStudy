44b24364ad54cf82ce7fe1afde7fc0deb48cd180
HanAtWork <wooyeophan@gmail.com>
2024-04-19 18:36:15 +0900

    기반만 넣음

--- structs/method_with_more_param/src/main.rs
+++ structs/method_with_more_param/src/main.rs
@@ -1,5 +1,26 @@
 // https://doc.rust-lang.org/book/ch05-03-method-syntax.html#methods-with-more-parameters
 
+struct Rectangle {
+    width: u32,
+    heigth: u32
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rect1 = Rectangle {
+        width: 30,
+        heigth: 54
+    };
+
+    let rect2 = Rectangle {
+        width: 50,
+        heigth: 30
+    };
+
+    let rect3 = Rectangle {
+        width: 40,
+        heigth: 20
+    };
+
+    println!("Can rect1 hold rect2? {}", false);
+    println!("Can rect1 hold rect3? {}", false);
 }
