fb4e635b12aba2784cc8e1f38a222abbcda5121a
HanAtWork <wooyeophan@gmail.com>
2024-04-22 16:59:02 +0900

    defining an enum 기반

---
+++ enums_and_pattern_matching/defining_an_enum/src/main.rs
@@ -0,0 +1,16 @@
+// https://doc.rust-lang.org/book/ch06-01-defining-an-enum.html
+
+/*
+Enum 은 가능한 변형들을 열거하는 방식으로 타입을 정의할 수 있게 해줌
+
+Struct는 관련된 필드랑 데이터를 한데 묶는 방식을 제공할 때, (Rectangle 의 width 와 height 같은)
+
+Enum은 어느 값이 가능한 값들의 묶음중 하나라는것을 의미함
+예를 들어 Rectangle 은 Circle과 Triangle도 포함되어 있는 모양 타입의 가능한 값중 하나라는걸 의미할 수 있음
+
+이런 기능을 위해 Rust는 이러한 가능성들을 enum 으로 인코딩함
+*/
+
+fn main() {
+    
+}
--- enums_and_pattern_matching/src/main.rs
+++
@@ -1,5 +0,0 @@
-// https://doc.rust-lang.org/book/ch06-00-enums.html
-
-fn main() {
-    
-}
