193e00be126b85e672e08e9b863b9b4883048c69
HanAtWork <wooyeophan@gmail.com>
2024-04-22 14:54:21 +0900

    파라미터 더 들어가는 메소드

--- structs/method_with_more_param/src/main.rs
+++ structs/method_with_more_param/src/main.rs
@@ -5,6 +5,14 @@ struct Rectangle {
     heigth: u32
 }
 
+impl Rectangle {
+    // 다른 언어랑 다른거 없음
+    // 오히려 TypeScript 랑 비슷하게 생김
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.heigth >= other.heigth
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
@@ -17,10 +25,10 @@ fn main() {
     };
 
     let rect3 = Rectangle {
-        width: 40,
+        width: 30,
         heigth: 20
     };
 
-    println!("Can rect1 hold rect2? {}", false);
-    println!("Can rect1 hold rect3? {}", false);
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
