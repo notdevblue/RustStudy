740d6469fcc406b9f69f6e55afeec984ef25ab83
HanAtWork <wooyeophan@gmail.com>
2024-05-02 17:52:53 +0900

    struct 를 사용한 저장

--- enums_and_pattern_matching/defining_an_enum/src/main.rs
+++ enums_and_pattern_matching/defining_an_enum/src/main.rs
@@ -24,9 +24,48 @@ v4와 v6둘다 기본적으론 IP 주소기 때문에 같은 타입으로 다뤄
 
 enum IpAddrKind {
     V4,
-    V6
+    V6,
+}
+// IpAddrKind enum은 이제 코드 어디에서든지 사용이 가능한 커스텀 타입임
+/*
+enum 안에 있는 요소들을 :: 를 통해 접근
+namespaced 되어 들어가 있는거임
+*/
+
+
+// 아직 IP 주소 자체를 저장하는 방법은 없기 때문에, 지금까지 배운 방법으로는 구조체를 통해 저장하는 방법을 택할 수 있음.
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn route(ip_kind: &IpAddrKind) {
+    match ip_kind {
+        IpAddrKind::V4 => println!("Routed IPv4"),
+        IpAddrKind::V6 => println!("Routed IPv6")
+    }
+}
+
+fn print_ipaddr(ip_addr: &IpAddr) {
+    route(&ip_addr.kind);
+    println!("Address: {}", ip_addr.address);
 }
 
 fn main() {
-    
+    // route(&IpAddrKind::V4);
+    // route(&IpAddrKind::V6);
+    // 이런 방식으로 넘길 수 있음
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    print_ipaddr(&home);
+    print_ipaddr(&loopback);
 }
